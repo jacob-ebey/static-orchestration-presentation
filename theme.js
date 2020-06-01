7d3e4fa1f93507e770396acd20b71e5b77f76c90
Jacob Ebey <jacob.ebey@live.com>
2020-06-01 13:35:24 -0700

    Initial commit

---
+++ theme.js
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    text: '#ffffff',
+    background: '#d31334',
+    primary: '#000000',
+  },
+  fonts: {
+    body: 'Calibre,Helvetica Neue,Helvetica,Roboto,Arial,sans-serif',
+    heading: 'Calibre,Helvetica Neue,Helvetica,Roboto,Arial,sans-serif',
+  },
+};
